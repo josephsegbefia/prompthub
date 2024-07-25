1154441bc9a271ef4a4370650747a2c754e5fe66
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-07-25 10:57:40 +0200

    Correct user account creation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,20 +5,21 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const fetchProviders = async () => {
       const response = await getProviders();
-
       setProviders(response);
     };
-    setProviders();
-  }, []);
 
+    fetchProviders();
+  }, []); // The empty dependency array ensures this runs only once
+
+  console.log(session);
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -34,7 +35,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -44,10 +45,10 @@ const Nav = () => {
             </button>
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
                 width={37}
                 height={37}
-                classNAme='rounded-full'
+                className='rounded-full'
                 alt='profile'
               />
             </Link>
@@ -71,13 +72,13 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user.image}
               width={37}
               height={37}
-              classNAme='rounded-full'
+              className='rounded-full'
               alt='profile'
               onClick={() => setToggleDropDown((prev) => !prev)}
             />
@@ -91,7 +92,7 @@ const Nav = () => {
                   My Profile
                 </Link>
                 <Link
-                  href='/cretae-prompt'
+                  href='/create-prompt'
                   className='dropdown_link'
                   onClick={() => setToggleDropDown(false)}
                 >
